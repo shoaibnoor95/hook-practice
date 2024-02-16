01d1cada1961b4ec0d9ae571c93f5e60224a704d
Shoaib <shoaib.noor@systemsltd.com>
2024-02-16 10:54:16 +0500

    minor change

--- script.js
+++ script.js
@@ -30,9 +30,10 @@ function fromDir(startPath, callback) {
 let found = false;
 fromDir(directoryToCheck, (filename) => {
   if (searchInFile(filename)) {
-    process.exit(1)
     console.log(`String found in file: ${filename}`);
     found = true;
+    process.exit(1)
+    
   }
 });
 
